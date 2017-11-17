fb56a52f1cacd9c64fee309f3ed1aa1dd5bd7a67
Gagan <gagan.nms@gmail.com>
2017-11-17 14:27:35 +0100

    Added new file Practise Python First sample Program

---
+++ Practise_add_multiply_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def multiply(x):
+    return (x*x)
+def add(x):
+    return (x+x)
+
+funcs = [multiply, add]
+for i in range(5):
+    value = list(map(lambda x: x(i), funcs))
+    print(value)
