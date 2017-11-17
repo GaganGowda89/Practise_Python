4f94f94ec2a5688fa2a15e02b5decd41c47ea225
Gagan <gagan.nms@gmail.com>
2017-11-17 19:45:27 +0100

    Python Program to find list of odd numbers from the range of left to right numbers given from user

---
+++ Practise_Python_2_range_odd.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#Program to List odd numbers in the given Range 
+
+import sys
+
+
+def  odd(x):
+     if (x % 2 != 0):
+     	odd_LIST.append(x)
+
+#user input 
+l, r =  input(" Enter the range, i.e left and right numbers (Note: Give a comma to enter the next no) :   ").split(',')
+#To use them as interger
+l, r = int (l), int(r)
+odd_LIST = []
+for i in range(l,r):
+    odd(i)
+for j in odd_LIST:
+	print (j)    
